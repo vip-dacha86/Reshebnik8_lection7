720f93498c79b01baa6d3235882ea9d8daed67e0
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-05 22:32:55 +0500

    task48_Dvumerniimassiv

---
+++ task48/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте двумерный массив размера m*n по формуле :a[i,j]=i+j Вывод массива на экран.
+using System;
+using static System.Console;
+Console.Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns );
+PrintArray (array);
+int[,]GetArray(int m, int n)
+{
+    int [,] result = new int [m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n ; j++)
+        {
+            result[i,j]=i+j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
