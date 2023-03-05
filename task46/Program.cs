a894ce587319c302c5ada2ce9f42e46e8c2d4a63
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-05 21:51:00 +0500

    task46_Dvumerniimassiv

---
+++ task46/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив двумерный размером m*n заполненный случайными числами.
+// mas = [1,2,3],[4,5,6,]
+using System;
+using static System.Console;
+Console.Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray (array);
+int[,]GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result =new int[m,n];
+    for (int i = 0; i < m; i++)
+{
+for(int j = 0; j < n; j++ )
+{
+    result[i, j] = new Random().Next(minValue,maxValue+1);
+}
+}
+return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0;i < inArray.GetLength(0);i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1);j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+    WriteLine();
+    }
+}
+
+
