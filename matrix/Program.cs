80e961f864071db3bdc61ae311b83674421bbe2c
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-06 14:44:06 +0500

    matrix

---
+++ matrix/Program.cs
@@ -0,0 +1,15 @@
+﻿Random r = new Random();
+
+Console.CursorVisible = false;
+while(true)
+{
+    Console.SetCursorPosition 
+    (
+    left: r.Next(Console.WindowWidth),
+    top: r.Next(Console.WindowHeight)
+    );
+    Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+}
+
+      
