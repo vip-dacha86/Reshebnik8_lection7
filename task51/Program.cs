6b9fa2fdbacbe9d9f846ae181d3489141ce92fc3
vip-dacha86 <vip-dacha86@yandex.ru>
2023-03-06 11:32:57 +0500

    task51_sum_diagonal_massiv

---
+++ task51/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив найдите сумму элементов,находящихся на главной диагонали с индексами (0,0);(1,1)и тд.
+
+using System;
+using static System.Console;
+
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine($"Сумма элементов главной диагонали = {GetSumDiag(array)}");
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max+1);
+        }
+
+    }
+    return result;
+}
+int GetSumDiag(int[,] array)
+{
+    int length = array.GetLength(0) < array.GetLength(1) ? array.GetLength(0) : array.GetLength(1);
+int sum = 0;
+  for (int i = 0; i < length; i++)
+{
+    sum += array[i,i];
+}
+return sum;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
